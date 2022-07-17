1510092c33bb40f12169bad06a91f07988caaa72
Astengo Gustavo da Silva <astengo@pop-os.localdomain>
2022-07-17 11:22:05 -0300

    final modulo 1

---
+++ pythontests/cores.py
@@ -0,0 +1,9 @@
+print('\033[4;30;41mTeste\033[m')
+print('\033[0;33;44mTeste\033[m')
+print('\033[1;35;43mTeste\033[m')
+print('\033[4;30;42mTeste\033[m')
+print('\033[mTeste\033[m')
+print('\033[7;30mTeste\033[m')
+
+nome = 'Pietro'
+print('Ola, muito prazer em te conhecer {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))
