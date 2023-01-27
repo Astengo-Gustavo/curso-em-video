98f9fd090c7164ea9e52956f7caab62bc950197b
Astengo Gustavo da Silva <astengo@pop-os.localdomain>
2023-01-27 07:06:46 -0300

    conclusão do curso

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -7,17 +7,22 @@ arq = 'cursoemvideo.txt'
 if not arqExiste(arq):
     criarArquivo(arq)
 
-
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if resposta == 1:
-        cabeçalho('opção 1')
+        # Opção de listar o conteúdo de um arquivo.
         leiaArquivo(arq)
     elif resposta == 2:
-        cabeçalho('opção 2')
+        # Opção de cadastrar uma pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = input('Nome: ')
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabeçalho('Saindo do sistema... até logo!')
         break
     else:
+        # digitou uma opção errada no menu.
         print('\033[31mERRO! Digite uma opção válida!\033[m')
     sleep(2)
